db422300f71df4b05fa0a3d1c18eec75e4cf5d83
yhonart <yusuf7789@gmail.com>
2022-12-28 20:39:11 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,6 @@
     <main>
         @yield('content')
     </main>
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="{{asset('bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('aos-master/dist/aos.js')}}"></script>
