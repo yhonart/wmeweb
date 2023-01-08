bdd97278d7444eac6a2bc52fe8f301b5a9358c95
yhonart <yusuf7789@gmail.com>
2023-01-08 22:49:21 +0700

    update index file

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ define('LARAVEL_START', microtime(true));
 if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
-
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
