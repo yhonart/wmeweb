c4bbd4e609507ea27d1ad53f761618e550363bf6
yhonart <yusuf7789@gmail.com>
2023-03-10 15:30:22 +0700

    update

--- resources/views/page/carier.blade.php
+++ resources/views/page/carier.blade.php
@@ -10,30 +10,69 @@
 <section class="page-bg">
     <div class="container py-5">
         <div class="row g-5">
-            <div class="col-md-4 col-12 mb-2 mt-5">
-                <h1 class="display-5 mb-0 text-primary">Operator Mesin CNC Milling & CNC Bubut</h1>
-            </div>   
-            <div class="col-md-8 col-12 mb-2 mt-5">
-                <div class="card border-0 rounded">
-                    <div class="card-header border-0">
-                        <h3 class="card-title">Kualifikasi</h3>                        
-                    </div>
+            <div class="col-12">
+                <div class="card card-primary">
                     <div class="card-body">
-                        <ul>
-                            <li class="border-bottom fs-5">Laki-laki</li>
-                            <li class="border-bottom fs-5">Pendidikan minimal SLTA/SMK (Jurusan Teknik Mesin)</li>
-                            <li class="border-bottom fs-5">Usia maksimal 27 Tahun</li>
-                            <li class="border-bottom fs-5">Berpengalaman minimal 2 Tahun</li>
-                            <li class="border-bottom fs-5">Dapat bekerja dengan team</li>
-                            <li class="border-bottom fs-5">Teliti, jujur dan tekun</li>
-                            <li class="border-bottom fs-5">Dapat membuat program CNC</li>
-                            <li class="border-bottom fs-5">Menguasai Autocad / Solidwork menjadi nilai tambah</li>
-                            <li class="border-bottom fs-5">Diutamakan domisili cilegon</li>
-                        </ul>
-                        <h5>Pendaftaran melalui : <a href="http://bit.ly/3YsOy1g" target="_blank" rel="noopener noreferrer">bit.ly/3YsOy1g</a>  <span class="fw-bold">Sebelum tanggal 25 Maret 2023</span></h5>
+                        <div class="row border-bottom border-info">
+                            <div class="col-md-4 col-12 mb-2 mt-5">
+                                <h1 class="display-5 mb-0 text-primary">Operator Mesin CNC Milling & CNC Bubut</h1>                                
+                            </div>   
+                            <div class="col-md-8 col-12 mb-2 mt-5">
+                                <div class="card border-0 rounded">
+                                    <div class="card-header border-0">
+                                        <h3 class="card-title">Kualifikasi</h3>                        
+                                    </div>
+                                    <div class="card-body">
+                                        <dl>
+                                            <dt>Operator Mesin CNC Milling</dt>
+                                            <dd>Untuk di tempatkan di Workshop Cilegon</dd>
+                                            <dt>Operator Mesin CNC Bubut</dt>
+                                            <dd>Untuk di tempatkan di Workshop Bekasi</dd>
+                                        </dl>
+                                        <ul>
+                                            <li class="border-bottom fs-5">Laki-laki</li>
+                                            <li class="border-bottom fs-5">Pendidikan minimal SLTA/SMK (Jurusan Teknik Mesin)</li>
+                                            <li class="border-bottom fs-5">Usia maksimal 27 Tahun</li>
+                                            <li class="border-bottom fs-5">Berpengalaman minimal 2 Tahun</li>
+                                            <li class="border-bottom fs-5">Dapat bekerja dengan team</li>
+                                            <li class="border-bottom fs-5">Teliti, jujur dan tekun</li>
+                                            <li class="border-bottom fs-5">Dapat membuat program CNC</li>
+                                            <li class="border-bottom fs-5">Menguasai Autocad / Solidwork menjadi nilai tambah</li>
+                                            <li class="border-bottom fs-5">Diutamakan domisili sesuai lokasi yang recruitment</li>
+                                        </ul>
+                                        <h5>Pendaftaran melalui : <a href="http://bit.ly/3YsOy1g" target="_blank" rel="noopener noreferrer">bit.ly/3YsOy1g</a>  <span class="fw-bold">Sebelum tanggal 25 Maret 2023</span></h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row border-bottom border-info">
+                            <div class="col-md-4 col-12 mb-2 mt-5">
+                                <h1 class="display-5 mb-0 text-primary">Marketing Engineering</h1>                                
+                            </div>   
+                            <div class="col-md-8 col-12 mb-2 mt-5">
+                                <div class="card border-0 rounded">
+                                    <div class="card-header border-0">
+                                        <h3 class="card-title">Kualifikasi</h3>                        
+                                    </div>
+                                    <div class="card-body">
+                                        <ul>
+                                            <li class="border-bottom fs-5">Laki-laki</li>
+                                            <li class="border-bottom fs-5">Pendidikan minimal SLTA/D3/S1</li>
+                                            <li class="border-bottom fs-5">Usia maksimal 30 Tahun</li>
+                                            <li class="border-bottom fs-5">Berpengalaman minimal 2 Tahun</li>
+                                            <li class="border-bottom fs-5">Memiliki komunikasi yang baik</li>
+                                            <li class="border-bottom fs-5">Memiliki relasi yang luas</li>
+                                            <li class="border-bottom fs-5">Teliti, Jujur dan Tekun</li>
+                                            <li class="border-bottom fs-5">Penempatan plan workshop Bekasi</li>
+                                        </ul>
+                                        <h5>Pendaftaran melalui : <a href="http://bit.ly/3YsOy1g" target="_blank" rel="noopener noreferrer">bit.ly/3YsOy1g</a>  <span class="fw-bold">Sebelum tanggal 25 Maret 2023</span></h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>         
+            </div>
         </div>        
     </div>
 </section>
